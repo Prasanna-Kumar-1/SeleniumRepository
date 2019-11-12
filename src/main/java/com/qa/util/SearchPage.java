eeacd943210fcc4a42dc420c1f6e5978f36576e9
Prasanna1312 <prasanna.kumar@outlook.com.au>
2019-11-12 17:03:48 +1100

    SearchPage featured added

---
+++ src/main/java/com/qa/util/SearchPage.java
@@ -0,0 +1,10 @@
+package com.qa.util;
+
+public class SearchPage {
+
+	public void searchMethod()
+	{
+		System.out.println("Search");
+	}
+	
+}
--- src/main/java/com/qa/util/TestBase.java
+++ src/main/java/com/qa/util/TestBase.java
@@ -1,5 +1,14 @@
 package com.qa.util;
 
 public class TestBase {
+	
+	public void test()
+	{
+		System.out.println("Test");
+	}
 
+	public void test2()
+	{
+		System.out.println("Test2");
+	}
 }
